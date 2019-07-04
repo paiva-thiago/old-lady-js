367a04284e1747c5fdf76bb257f810c03c76e48c
paiva-thiago <paiva.thiago@gmail.com>
2019-07-04 15:39:00 -0300

    Já sabendo quando termina o jogo!

--- js/scripts.js
+++ js/scripts.js
@@ -1,44 +1,61 @@
-/** Vamos criar uma variável para identificar quem vai para o campo, X ou O. Começaremos com X. */
 var jogadorAtivo = 'X';
-
 /**
- * Vamos criar agora uma função para preencher o campo. O this ('isto' em inglês) é o elemento HTML (no caso, o quadro) que será atrelado à função.
- * innerHTML é o conteúdo entre as tags do elemento HTML relacionado. Vamos chamar este cara lá embaixo:
+ * Lembra que falamos da matriz no último commit? pois bem, vamos criar. Se você não conseguiu pensar numa forma de resolver, talvez visualizando a matriz abaixo fique mais fácil :)
+ * Por enquanto vamos fazer uma função para verificar se o jogo acabou, depois vamos aplicar quem ganhou.
  */
+var tabuleiro = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+]
+/** A função vai varrer o tabuleiro procurando por algum valor com null se tiver, o jogo não acabou */
+function jogoAcabou(){
+    for(let l=0;l<tabuleiro.length;l++){
+        for(let c=0;c<tabuleiro[l].length;c++){
+            console.log(l+'-'+c+'='+tabuleiro[l][c]);
+            if(tabuleiro[l][c]===0){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+//Vamos criar uma função para obter a linha e coluna do elemento clicado. Veja no index.html que no atributo id eu tenho como valor velha-{linha}-{coluna}
+//Vamos retornar como um vetor, onde a primeira posição é o numero da linha e a segunda o número da coluna
+function linhaColunaDoClicado(elemento){
+    //Pego o atributo id através do método getAttribute, (traduzindo, pegar Atributo)
+    let id = elemento.getAttribute('id');
+    //agora vamos separar os valores pelo traço através do método disponível em valores string chamado split. Ela me retorna um vetor a partir do caracter que eu separei.
+    let separados = id.split('-');
+    // Com isto, um id 'velha-0-0' me retornará em separados ['velha',0,0]. O primeiro item não será útil, então vou excluir, para que só reste os dois últimos.
+    // Vamos usar a função disponível em vetor chamada shift()
+    separados.shift();
+    return separados;
+}
 function jogar(){
-    //Primeiro, não posso preencher um campo que já foi utilizado, logo eu devo verificar se ele é diferente de 'X' E diferente de O
+    //Primeiro, não posso preencher um campo que já foi utilizado, logo eu devo verificar se ele é diferente de 'X' E diferente de O    
     if((this.innerHTML!='X')&&(this.innerHTML!='O')){
         this.innerHTML=jogadorAtivo;
-        /** Jogada feita, vamos alternar o jogador com uma estrutura condicional */
-        if(jogadorAtivo==='X'){
-            jogadorAtivo='O';
+        //Agora vamos atualizar nossa matriz
+        let linhaColuna = linhaColunaDoClicado(this);
+        console.log(JSON.stringify(linhaColuna));
+        let linha   = linhaColuna[0];
+        let coluna  = linhaColuna[1];
+        tabuleiro[linha][coluna]=jogadorAtivo;
+        console.log(tabuleiro);
+        /** Jogada feita, só vamos alternar o jogador se o jogo acabar */
+        if(jogoAcabou()){
+            alert('Fim de Jogo! Mas não sei quem ganhou!');
         }else{
-            jogadorAtivo='X';
+            //Agora vamos trocar aquele if por uma estrutura menor chamada OPERADOR TERNÁRIO
+            //Ele possui três parâmetros: ({condição} ? {valor se condição for verdadeira} : {valor se condição for falsa} )
+            jogadorAtivo = ((jogadorAtivo==='X') ? 'O' : 'X');            
         }
+        
     }
 }
 
-/** 
- * Agora, vamos botar o Document Object Model para funcionar. Ele visualiza cada elemento do HTML como um objeto. Por exemplo:
- * document me retornará o <body> todo
- * document.getElementById('item') me retornará o elemento com o atributo id igual a 'item'. Se não tiver nenhum me retornará undefined.
- *  
- * E temos uma forma para atrelar eventos ao elemento, usando o addEventListener. Ele tem dois parâmetros, uma string que identifica o evento e uma função que atrela ao mesmo.
- * 
- * Vamos jogar nos caras. Depois vamos ver uma forma bem mais simples com outros métodos do DOM
-*/
-
-document.getElementById('velha-1-1').addEventListener('click',jogar);
-document.getElementById('velha-1-2').addEventListener('click',jogar);
-document.getElementById('velha-1-3').addEventListener('click',jogar);
-document.getElementById('velha-2-1').addEventListener('click',jogar);
-document.getElementById('velha-2-2').addEventListener('click',jogar);
-document.getElementById('velha-2-3').addEventListener('click',jogar);
-document.getElementById('velha-3-1').addEventListener('click',jogar);
-document.getElementById('velha-3-2').addEventListener('click',jogar);
-document.getElementById('velha-3-3').addEventListener('click',jogar);
-/**
- * Com isso, nosso quadro já pode ser preenchido, mas precisamos pensar em como terminar. Quando um jogo da velha termina? Como eu aplico esta condição no Javascript?
- * Dica para isto: Vamos lembrar do conceito de matrizes. Uma matriz 3x3 é familiar com nosso tabuleiro?
- */
-
+var velhas = document.getElementsByClassName('velha-field');
+for(let x=0;x<velhas.length;x++){
+    velhas[x].addEventListener('click',jogar);
+}
